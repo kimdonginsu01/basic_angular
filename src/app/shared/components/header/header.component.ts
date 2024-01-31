6a831f7c905ef13eabbabf53133aa2d97f8113a9
tuyenhm <tuyenhm@arrowhitech.com>
2024-01-31 18:04:24 +0700

    Update module 4

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,18 +1,21 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { AuthService } from '../../../auth/auth.service';
-import { MenuItem } from 'primeng/api';
-import { MenubarModule } from 'primeng/menubar';
-import { ButtonModule } from 'primeng/button';
-import { Router } from '@angular/router';
-import { CartService } from '../../services/cart.service';
-import { BadgeModule } from 'primeng/badge';
 import { CommonModule } from '@angular/common';
-import { OverlayPanel, OverlayPanelModule } from 'primeng/overlaypanel';
-import { DropdownModule } from 'primeng/dropdown';
-import { TableModule } from 'primeng/table';
-import { InputNumberInputEvent, InputNumberModule } from 'primeng/inputnumber';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { Subject, debounceTime, switchMap } from 'rxjs';
+import { Router } from '@angular/router';
+import { ConfirmationService, MenuItem, MessageService } from 'primeng/api';
+import { BadgeModule } from 'primeng/badge';
+import { ButtonModule } from 'primeng/button';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { DropdownModule } from 'primeng/dropdown';
+import { InputNumberInputEvent, InputNumberModule } from 'primeng/inputnumber';
+import { MenubarModule } from 'primeng/menubar';
+import { OverlayPanel, OverlayPanelModule } from 'primeng/overlaypanel';
+import { TableModule } from 'primeng/table';
+import { ToastModule } from 'primeng/toast';
+import { Subject, debounceTime, of, switchMap } from 'rxjs';
+import { AuthService } from '../../../auth/auth.service';
+import { CartService } from '../../services/cart.service';
+import { ConfirmPopupModule } from 'primeng/confirmpopup';
 
 @Component({
   selector: 'app-header',
@@ -27,15 +30,20 @@ import { Subject, debounceTime, switchMap } from 'rxjs';
     TableModule,
     InputNumberModule,
     FormsModule,
+    ConfirmDialogModule,
+    ToastModule,
+    ConfirmPopupModule,
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
+  providers: [ConfirmationService, MessageService],
 })
 export class HeaderComponent implements OnInit {
   @ViewChild(OverlayPanel) private op!: OverlayPanel;
   items: MenuItem[];
   cartItems: any = [];
   totalPrice: number = 0;
+  currentUserId: number;
 
   private updateCartSubject = new Subject<{
     event: InputNumberInputEvent;
@@ -45,8 +53,12 @@ export class HeaderComponent implements OnInit {
   constructor(
     public authService: AuthService,
     public router: Router,
-    public cartService: CartService
+    public cartService: CartService,
+    private confirmationService: ConfirmationService,
+    private messageService: MessageService
   ) {
+    const { id } = this.authService.getCurrentUser();
+    this.currentUserId = id;
     this.items = [
       { label: 'Home', routerLink: '/home' },
       { label: 'Books', routerLink: '/books' },
@@ -57,25 +69,31 @@ export class HeaderComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    const { id } = this.authService.getCurrentUser();
     this.cartService.cart$.subscribe((value: any) => {
       this.cartItems = value;
       this.totalPrice = this.calculateTotalPrice(this.cartItems);
     });
 
-    this.getUserCart(id);
+    this.getUserCart(this.currentUserId);
 
     this.updateCartSubject
       .pipe(
         debounceTime(300),
         switchMap(({ event, productId }) => {
-          return this.cartService.updateCartProduct(productId, {
-            quantity: event.value,
-          });
+          if (event.value && +event.value <= 20) {
+            return this.cartService.updateCartProduct(productId, {
+              quantity: event.value,
+            });
+          } else {
+            return of(this.cartItems);
+          }
         }),
-        switchMap(() => {
-          const { id } = this.authService.getCurrentUser();
-          return this.cartService.getUserCart(id);
+        switchMap((cartItems) => {
+          if (Array.isArray(cartItems)) {
+            return of(cartItems);
+          } else {
+            return this.cartService.getUserCart(this.currentUserId);
+          }
         })
       )
       .subscribe((updatedCart) => {
@@ -110,5 +128,32 @@ export class HeaderComponent implements OnInit {
 
   handleUpdateCart(event: InputNumberInputEvent, productId: number) {
     this.updateCartSubject.next({ event, productId });
+  }
+
+  handleDeleteCartItem(event: Event, userId: number) {
+    event.stopPropagation();
+    console.log(userId)
+    this.confirmationService.confirm({
+      target: event.target as EventTarget,
+      message: 'Do you want to delete this item?',
+      header: 'Delete Confirmation',
+      icon: 'pi pi-info-circle',
+      acceptButtonStyleClass: 'p-button-danger p-button-text',
+      rejectButtonStyleClass: 'p-button-text p-button-text',
+      acceptIcon: 'none',
+      rejectIcon: 'none',
+
+      accept: () => {
+        // this.cartService.deleteCartItem(productId).subscribe(() => {
+        //   this.messageService.add({
+        //     severity: 'info',
+        //     summary: 'Confirmed',
+        //     detail: 'Item deleted',
+        //   });
+        //   // Refresh the cart after deletion
+        //   this.getUserCart(this.currentUserId);
+        // });
+      },
+    });
   }
 }
